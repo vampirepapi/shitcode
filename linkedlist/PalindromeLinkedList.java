96d9464c667162e97cab9ad36827843f9b685d75
vampirepapi <shubhamsourabh8@gmail.com>
2024-03-07 02:11:34 +0530

    solve palindrome ll problem and documented some past ac sols

---
+++ linkedlist/PalindromeLinkedList.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode nextCurr = null;
+
+        while (curr!=null) {
+            nextCurr = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextCurr;
+        }
+        return prev;
+    }
+}
--- linkedlist/Solution.java
+++ linkedlist/Solution.java
@@ -1,31 +1,75 @@
 package linkedlist;
 
-import java.util.Stack;
 
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        this.data = x;
+        this.next = null;
+    }
+
+    // Constructor that accepts a dataue and a Node object for the next variable
+    Node(int x, Node next) {
+        this.data = x;
+        this.next = next;
+    }
+
+}
+
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode curr = head;
-        ListNode nextCurr = null;
-        ListNode prev = null;
+    public boolean isPalindrome(Node head) {
+        if(head==null || head.next == null){
+            return true;
+        }
 
-        while (curr!=null) {
-            nextCurr=curr.next;
-            curr.next=prev;
-            prev=curr;
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        Node newNode = reverseALL(slow);
+        Node startNode = head;
+
+        while (newNode != null) {
+            if(newNode.data != startNode.data){
+                reverseALL(newNode);
+                return false;
+            }
+            newNode = newNode.next;
+            startNode = startNode.next;
+        }
+
+        reverseALL(newNode);
+        return true;
+    }
+
+    public static Node reverseALL(Node head){
+        Node curr = head;
+        Node prev = null;
+        Node nextCurr = null;
+        while (curr != null) {
+            nextCurr = curr.next;
+            curr.next = prev;
+            prev = curr;
             curr = nextCurr;
-            
         }
         return prev;
+    }
+    public static void main(String[] args) {
+        Node node4 = new Node(1);
+        Node node3 = new Node(2);
+        Node node2 = new Node(2);
+        Node node1 = new Node(1);
 
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        Solution sb = new Solution();
+        System.out.println(sb.isPalindrome(node1));
     }
 }
