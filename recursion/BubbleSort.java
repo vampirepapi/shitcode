e1ca009eb460c0a8a87ce06fdacd041dd2e8c5d4
vampirepapi <shubhamsourabh8@gmail.com>
2024-08-07 04:03:08 +0530

    bubble sort

---
+++ recursion/BubbleSort.java
@@ -0,0 +1,20 @@
+public class BubbleSort {
+
+    public int[] sortArray(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            boolean flag = false;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                break;
+            }
+        }
+        return nums;
+    }
+}
