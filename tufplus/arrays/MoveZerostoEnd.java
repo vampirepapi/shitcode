0a17fcfdb382f8bdd20d823052537d0101a2a98c
vampirepapi <shubhamsourabh8@gmail.com>
2024-09-04 00:34:02 +0530

    changes

---
+++ tufplus/arrays/MoveZerostoEnd.java
@@ -0,0 +1,13 @@
+package tufplus.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveZerostoEnd {
+    public void moveZeroes(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+
+
+    }
+}
