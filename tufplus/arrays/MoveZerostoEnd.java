25b79a20d06091203aaa3c4d100a4aca432e7341
vampirepapi <shubhamsourabh8@gmail.com>
2024-09-04 00:34:04 +0530

    test

--- tufplus/arrays/MoveZerostoEnd.java
+++ tufplus/arrays/MoveZerostoEnd.java
@@ -1,13 +1,28 @@
 package tufplus.arrays;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MoveZerostoEnd {
+class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> ans = new ArrayList<>();
 
+        // init i to first zero valued element
+        int i = -1;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == 0) {
+                i = j;
+                break;
+            }
+        }
 
+        // if i is still -1, then no zero element found
+        if (i == -1) return;
 
+        // iterate from i+1 to end of array
+        for (int j = i + 1; j < nums.length; j++) {
+            // if non-zero element found, swap with i
+            if (nums[j] != 0) {
+                nums[i] = nums[j];
+                nums[j] = 0;
+                i++;
+            }
+        }        
     }
 }
